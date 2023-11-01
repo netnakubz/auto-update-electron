dcd9716f2e200890c801886d5701513f1ee20629
kanit <pkanit401@gmail.com>
2023-11-02 02:45:41 +0700

    version 1.0.7

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { autoUpdater } = require('electron-updater');
+const log = require('electron-log');
+
 let mainWindow;
 
 function createWindow() {
@@ -26,43 +28,92 @@ function createWindow() {
    mainWindow.webContents.openDevTools();
 }
 
-app.on('ready', () => {
-    createWindow();
-});
-
-app.on('window-all-closed', function () {
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', function () {
+    createWindow()
+  })
+  
+  // Quit when all windows are closed.
+  app.on('window-all-closed', function () {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
-        app.quit();
+      app.quit()
     }
-});
-
-app.on('activate', function () {
+  })
+  
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
-        createWindow();
+      createWindow()
     }
-});
-
-ipcMain.on('app_version', (event) => {
-    event.sender.send('app_version', { version: app.getVersion() });
-});
-
-autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-});
-
-ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-});
-
-autoUpdater.on('update-downloaded', (ev, info) => {
+  })
+  
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and require them here.
+  
+  function sendStatusToWindow(text) {
+    log.info(text);
+    mainWindow.webContents.send('message', text);
+  }
+  
+  autoUpdater.on('checking-for-update', () => {
+    sendStatusToWindow('Checking for update...');
+  })
+  autoUpdater.on('update-available', (info) => {
+    sendStatusToWindow('Update available.');
+  })
+  autoUpdater.on('update-not-available', (info) => {
+    sendStatusToWindow('Update not available.');
+  })
+  autoUpdater.on('error', (err) => {
+    sendStatusToWindow('Error in auto-updater. ' + err);
+  })
+  autoUpdater.on('download-progress', (progressObj) => {
+    let log_message = "Download speed: " + progressObj.bytesPerSecond;
+    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+    sendStatusToWindow(log_message);
+  })
+  autoUpdater.on('update-downloaded', (info) => {
+    sendStatusToWindow('Update downloaded');
+  });
+  
+  
+  
+  //-------------------------------------------------------------------
+  // Auto updates
+  //
+  // For details about these events, see the Wiki:
+  // https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
+  //
+  // The app doesn't need to listen to any events except `update-downloaded`
+  //
+  // Uncomment any of the below events to listen for them.  Also,
+  // look in the previous section to see them being used.
+  //-------------------------------------------------------------------
+  // autoUpdater.on('checking-for-update', () => {
+  // })
+  // autoUpdater.on('update-available', (ev, info) => {
+  // })
+  // autoUpdater.on('update-not-available', (ev, info) => {
+  // })
+  // autoUpdater.on('error', (ev, err) => {
+  // })
+  // autoUpdater.on('download-progress', (ev, progressObj) => {
+  // })
+  autoUpdater.on('update-downloaded', (ev, info) => {
     // Wait 5 seconds, then quit and install
     // In your application, you don't need to wait 5 seconds.
     // You could call autoUpdater.quitAndInstall(); immediately
     setTimeout(function () {
-        autoUpdater.quitAndInstall();
+      autoUpdater.quitAndInstall();
     }, 5000)
-})
-
-app.on('ready', function () {
+  })
+  
+  app.on('ready', function () {
     autoUpdater.checkForUpdates();
-});
+  });
